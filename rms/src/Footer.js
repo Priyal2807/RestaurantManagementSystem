a57cfb686a834fc6e161d2e238887b67d37b5d4a
Priyal2807 <priyalsobti@gmail.com>
2022-07-04 22:07:33 +0530

    had to make changes to compensate for materialize css

--- rms/src/Footer.js
+++ rms/src/Footer.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <nav className="nav-wrapper blue darken-3">
-            <div className="container">
+        <nav className="nav-div">
+            
                 <ul className="right">
                     <li>@call - 34546765</li>
                     <li>@email - rms@example.com</li>
                 </ul>
-            </div>
+           
         </nav>
     )
 }
